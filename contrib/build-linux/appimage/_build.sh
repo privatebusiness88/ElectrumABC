6dc3bcf396fabae1dfb0906a77e2b525808b5d14
PiRK <pierrekn@gmail.com>
2020-11-20 08:46:14 +0100

    update program name in .desktop file

--- contrib/build-linux/appimage/_build.sh
+++ contrib/build-linux/appimage/_build.sh
@@ -6,7 +6,7 @@ PROJECT_ROOT="$(dirname "$(readlink -e "$0")")/../../.."
 CONTRIB="$PROJECT_ROOT/contrib"
 DISTDIR="$PROJECT_ROOT/dist"
 BUILDDIR="$CONTRIB/build-linux/appimage/build/appimage"
-APPDIR="$BUILDDIR/Electron-Cash.AppDir"
+APPDIR="$BUILDDIR/ElectrumBCHA.AppDir"
 CACHEDIR="$CONTRIB/build-linux/appimage/.cache/appimage"
 PYDIR="$APPDIR"/usr/lib/python3.6
 
@@ -21,7 +21,7 @@ PKG2APPIMAGE_COMMIT="eb8f3acdd9f11ab19b78f5cb15daa772367daf15"
 
 
 VERSION=`git describe --tags --dirty --always`
-APPIMAGE="$DISTDIR/Electron-Cash-$VERSION-x86_64.AppImage"
+APPIMAGE="$DISTDIR/ElectrumBCHA-$VERSION-x86_64.AppImage"
 
 rm -rf "$BUILDDIR"
 mkdir -p "$APPDIR" "$CACHEDIR" "$DISTDIR"
