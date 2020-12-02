01f85fc2ceeb10f649e16f3f6094bffb8ef5b35e
PiRK <pierrekn@gmail.com>
2020-12-02 16:57:13 +0100

    don't repeat the repository name and owner

--- electroncash/constants.py
+++ electroncash/constants.py
@@ -4,8 +4,12 @@ from collections import OrderedDict
 PROJECT_NAME: str = "Electrum ABC"
 PROJECT_NAME_NO_SPACES = "ElectrumABC"
 SCRIPT_NAME: str = "electrum-abc"
-REPOSITORY_URL: str = "https://github.com/Bitcoin-ABC/ElectrumABC"
-RELEASES_JSON_URL: str = "https://raw.github.com/Bitcoin-ABC/ElectrumABC/master/contrib/update_checker/releases.json"
+REPOSITORY_OWNER: str = "Bitcoin-ABC"
+REPOSITORY_NAME: str = "ElectrumABC"
+REPOSITORY_URL: str = f"https://github.com/{REPOSITORY_OWNER}/{REPOSITORY_NAME}"
+RELEASES_JSON_URL: str = f"https://raw.github.com/{REPOSITORY_OWNER}/" \
+                         f"{REPOSITORY_NAME}/master/contrib/update_checker" \
+                         f"/releases.json"
 
 POSIX_DATA_DIR: str = ".electrum-abc"
 """This is the name of the directory where the wallets, recent server lists
