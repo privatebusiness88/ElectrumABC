c85e4e0cae195d50cd5798c376deddd60adb4c1d
PiRK <pierrekn@gmail.com>
2021-10-26 14:57:59 +0200

    fix flake8 F403 warnings

--- electroncash_gui/qt/utils/darkdetect/__init__.py
+++ electroncash_gui/qt/utils/darkdetect/__init__.py
@@ -10,13 +10,13 @@ import sys
 import platform
 
 if sys.platform != "darwin":
-    from ._dummy import *
+    from ._dummy import isDark, isLight, theme
 else:
     from distutils.version import LooseVersion as V
     if V(platform.mac_ver()[0]) < V("10.14"):
-        from ._dummy import *
+        from ._dummy import isDark, isLight, theme
     else:
-        from ._detect import *
+        from ._detect import isDark, isLight, theme
     del V
 
 del sys, platform
