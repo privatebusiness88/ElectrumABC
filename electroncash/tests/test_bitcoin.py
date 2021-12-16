7c189c31286110a4a20dbdb4a0ce48e59e360626
Calin Culianu <calin.culianu@gmail.com>
2021-12-16 00:18:49 -0600

    Fix bitcoin.py `push_script` to implement BIP62 correctly
    Pushes need to be "minimally encoded" see:
    https://en.bitcoin.it/wiki/BIP_0062#Push_operators
    
    This is a low-impact change, and is only here for correctness to prevent
    EC from creating non-standard txns. This change only affects pushes of
    size 0 or size 1, which Electron Cash doesn't normally ever push,
    however if some plugin or some client code requires push_script to push
    tiny 1-byte scripts, now the non-standardness bug has been addressed.
    
    Also in this commit: Move the "OpCodes" enum to bitcoin.py.

--- electroncash/tests/test_bitcoin.py
+++ electroncash/tests/test_bitcoin.py
@@ -9,6 +9,7 @@ from ..bitcoin import (
     EC_KEY,
     Bip38Key,
     Hash,
+    OpCodes,
     SignatureType,
     address_from_private_key,
     bip32_private_derivation,
@@ -26,6 +27,7 @@ from ..bitcoin import (
     point_to_ser,
     public_key_from_private_key,
     public_key_to_p2pkh,
+    push_script,
     pw_decode,
     pw_encode,
     regenerate_key,
@@ -36,7 +38,7 @@ from ..bitcoin import (
     xpub_type,
 )
 from ..networks import set_mainnet, set_testnet
-from ..util import bfh
+from ..util import bfh, bh2u
 
 try:
     import ecdsa
@@ -240,6 +242,35 @@ class Test_bitcoin(unittest.TestCase):
         self.assertEqual(op_push(0xFFFF), "4dffff")
         self.assertEqual(op_push(0x10000), "4e00000100")
         self.assertEqual(op_push(0x12345678), "4e78563412")
+
+    def test_push_script(self):
+        # https://github.com/bitcoin/bips/blob/master/bip-0062.mediawiki#push-operators
+        self.assertEqual(push_script(""), bh2u(bytes([OpCodes.OP_0])))
+        self.assertEqual(push_script("07"), bh2u(bytes([OpCodes.OP_7])))
+        self.assertEqual(push_script("10"), bh2u(bytes([OpCodes.OP_16])))
+        self.assertEqual(push_script("81"), bh2u(bytes([OpCodes.OP_1NEGATE])))
+        self.assertEqual(push_script("11"), "0111")
+        self.assertEqual(push_script(75 * "42"), "4b" + 75 * "42")
+        self.assertEqual(
+            push_script(76 * "42"),
+            bh2u(bytes([OpCodes.OP_PUSHDATA1]) + bfh("4c" + 76 * "42")),
+        )
+        self.assertEqual(
+            push_script(100 * "42"),
+            bh2u(bytes([OpCodes.OP_PUSHDATA1]) + bfh("64" + 100 * "42")),
+        )
+        self.assertEqual(
+            push_script(255 * "42"),
+            bh2u(bytes([OpCodes.OP_PUSHDATA1]) + bfh("ff" + 255 * "42")),
+        )
+        self.assertEqual(
+            push_script(256 * "42"),
+            bh2u(bytes([OpCodes.OP_PUSHDATA2]) + bfh("0001" + 256 * "42")),
+        )
+        self.assertEqual(
+            push_script(520 * "42"),
+            bh2u(bytes([OpCodes.OP_PUSHDATA2]) + bfh("0802" + 520 * "42")),
+        )
 
 
 class Test_bitcoin_testnet(unittest.TestCase):
