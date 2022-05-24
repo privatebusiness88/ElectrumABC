a2cbf32bdaf15505a4042b5d2f311dbaf42f79e0
PiRK <pierrekn@gmail.com>
2022-05-24 11:28:59 +0200

    implement exchange rate API
    Add widgets to select an API for exchange rate fetching;

--- electroncash_gui/qt/invoice_dialog.py
+++ electroncash_gui/qt/invoice_dialog.py
@@ -24,7 +24,8 @@
 # SOFTWARE.
 from __future__ import annotations
 
-from typing import Optional
+import enum
+from typing import List, Optional, Type
 
 from PyQt5 import QtCore, QtWidgets
 
@@ -49,6 +50,8 @@ class InvoiceDialog(QtWidgets.QDialog):
         layout.addWidget(self.exchange_rate_widget)
         layout.addSpacing(10)
 
+        layout.addStretch(1)
+
         # Trigger callback to init widgets
         self._on_currency_changed(self.amount_currency_edit.get_currency())
 
@@ -57,7 +60,7 @@ class InvoiceDialog(QtWidgets.QDialog):
 
     def _on_currency_changed(self, currency: str):
         self.exchange_rate_widget.setVisible(currency.lower() != "xec")
-        self.exchange_rate_widget.setCurrency(currency)
+        self.exchange_rate_widget.set_currency(currency)
 
 
 class AmountCurrencyEdit(QtWidgets.QWidget):
@@ -67,6 +70,7 @@ class AmountCurrencyEdit(QtWidgets.QWidget):
         super().__init__(parent)
         layout = QtWidgets.QVBoxLayout()
         self.setLayout(layout)
+        layout.setContentsMargins(0, 0, 0, 0)
 
         layout.addWidget(QtWidgets.QLabel("Amount"))
         amount_layout = QtWidgets.QHBoxLayout()
@@ -94,7 +98,9 @@ class ExchangeRateWidget(QtWidgets.QWidget):
         super().__init__(parent)
         layout = QtWidgets.QVBoxLayout()
         self.setLayout(layout)
+        layout.setContentsMargins(0, 0, 0, 0)
 
+        layout.addWidget(QtWidgets.QLabel("Exchange rate"))
         fixed_rate_layout = QtWidgets.QHBoxLayout()
         layout.addLayout(fixed_rate_layout)
         self.fixed_rate_rb = QtWidgets.QRadioButton("Fixed rate")
@@ -103,11 +109,129 @@ class ExchangeRateWidget(QtWidgets.QWidget):
         self.rate_edit.setDecimals(8)
         self.rate_edit.setRange(10**-8, 10**100)
         self.rate_edit.setStepType(QtWidgets.QAbstractSpinBox.AdaptiveDecimalStepType)
-        self.rate_edit.setValue(1.0)
         fixed_rate_layout.addWidget(self.rate_edit)
 
-    def setCurrency(self, currency: str):
+        api_rate_layout = QtWidgets.QVBoxLayout()
+        layout.addLayout(api_rate_layout)
+        self.api_rate_rb = QtWidgets.QRadioButton("Fetch the rate at payment time")
+        api_rate_layout.addWidget(self.api_rate_rb)
+
+        self.api_widget = ExchangeRateAPIWidget()
+        margins = self.api_widget.contentsMargins()
+        margins.setLeft(margins.left() + 10)
+        self.api_widget.setContentsMargins(margins)
+
+        api_rate_layout.addWidget(self.api_widget)
+
+        # Signals
+        self.api_rate_rb.toggled.connect(self._on_api_rate_clicked)
+
+        # Default state
+        # Use an exclusive button group to disallow unckecking the check radio button
+        self._button_group = QtWidgets.QButtonGroup()
+        self._button_group.setExclusive(True)
+        self._button_group.addButton(self.fixed_rate_rb)
+        self._button_group.addButton(self.api_rate_rb)
+
+        self.api_widget.setVisible(False)
+        self.fixed_rate_rb.setChecked(True)
+        self.rate_edit.setValue(1.0)
+
+    def set_currency(self, currency: str):
         self.fixed_rate_rb.setText(f"Fixed rate ({currency}/XEC)")
+        self.api_widget.set_currency(currency)
+
+    def _on_api_rate_clicked(self, is_checked: bool):
+        self.api_widget.setVisible(is_checked)
+
+
+class APIDataFormat(enum.Enum):
+    JSON = 1
+    YAML = 2
+
+
+class ExchangeRateAPI:
+    def __init__(self, currency: str):
+        self._currency = currency
+        self.url: str = ""
+        self.format: APIDataFormat = APIDataFormat.JSON
+        self.keys: List[str] = []
+
+
+class CoingeckoAPI1(ExchangeRateAPI):
+    def __init__(self, currency: str):
+        super().__init__(currency)
+        self.url = f"https://api.coingecko.com/api/v3/simple/price?ids=ecash&vs_currencies={currency.lower()}"
+        self.format = APIDataFormat.JSON
+        self.keys = ["ecash", f"{currency.lower()}"]
+
+
+class CoingeckoAPI2(ExchangeRateAPI):
+    def __init__(self, currency: str):
+        super().__init__(currency)
+        self.url = "https://api.coingecko.com/api/v3/coins/ecash?localization=False&sparkline=false"
+        self.format = APIDataFormat.JSON
+        self.keys = ["market_data", "current_price", f"{currency.lower()}"]
+
+
+APIS: List[Type[ExchangeRateAPI]] = [CoingeckoAPI1, CoingeckoAPI2]
+
+
+class ExchangeRateAPIWidget(QtWidgets.QWidget):
+    def __init__(self, parent: Optional[QtWidgets.QWidget] = None):
+        super().__init__(parent)
+
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
+        layout.addWidget(QtWidgets.QLabel("Request URL"))
+        self.request_url_edit = QtWidgets.QComboBox()
+        self.request_url_edit.setEditable(True)
+        layout.addWidget(self.request_url_edit)
+
+        layout.addWidget(QtWidgets.QLabel("Data format"))
+        data_format_layout = QtWidgets.QHBoxLayout()
+        layout.addLayout(data_format_layout)
+
+        self.json_rb = QtWidgets.QRadioButton("JSON")
+        data_format_layout.addWidget(self.json_rb)
+        self.yaml_rb = QtWidgets.QRadioButton("YAML")
+        data_format_layout.addWidget(self.yaml_rb)
+
+        layout.addWidget(QtWidgets.QLabel("Keys"))
+        self.keys_edit = QtWidgets.QLineEdit()
+        layout.addWidget(self.keys_edit)
+
+        # Default state
+        self._currency: str = ""
+        self.set_currency("USD")
+        self.json_rb.setChecked(True)
+
+        # signals
+        self.request_url_edit.currentIndexChanged.connect(self._on_api_url_selected)
+
+    def set_currency(self, currency: str):
+        self._currency = currency
+        # Update currency part of preset URLs while remembering selection
+        index = self.request_url_edit.currentIndex()
+        self.request_url_edit.clear()
+        apis: List[ExchangeRateAPI] = []
+        for api_class in APIS:
+            api = api_class(currency)
+            self.request_url_edit.addItem(api.url)
+            apis.append(api)
+        self.request_url_edit.setCurrentIndex(index)
+
+    def _on_api_url_selected(self, index: int):
+        if index < 0:
+            self.keys_edit.clear()
+            return
+        api = APIS[index](self._currency)
+        self.keys_edit.setText(", ".join(api.keys))
+        if api.format == APIDataFormat.JSON:
+            self.json_rb.setChecked(True)
+        else:
+            self.yaml_rb.setChecked(True)
 
 
 if __name__ == "__main__":
