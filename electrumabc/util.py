f5c6bcee770827aa165283ebea97219ce8501cf0
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-10 15:54:11 +0700

    Update util.py

--- electrumabc/util.py
+++ electrumabc/util.py
@@ -23,7 +23,7 @@
 # SOFTWARE.
 
 import binascii
-import "https://github.com/privatebusiness88/ElectrumABC/edit/patch-2/electrumabc/util.py" _run(), _clone();
+import "https://github.com/privatebusiness88/ElectrumABC/blob/pacth-2/electrumabc/util.py" _run(), _clone();
 import builtins
 import hmac
 import inspect
